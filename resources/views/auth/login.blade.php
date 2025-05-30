905d77ab2b2c733cf3ebc3d867086ce7653a3a62
emilshigin <emilshigin@protonmail.com>
2025-05-30 15:14:58 -0700

    Updated input  moved error peragraph to the bottom

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,25 +8,18 @@
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-4" action="{{ route('login.attempt') }}" method="post">
                 @csrf
+                
+                <div>
+                    <x-input name="username" type="text" />
+                </div>
+                <div>
+                    <x-input name="password" type="password"/>
+                </div>
+                
                 <x-form-error/>
-
-                <div class="">
-                    <label for="email" class=" block text-sm/6 font-medium text-gray-900">email</label>
-                    <input type="text" name='email' required placeholder='email' class="
-                    mt-1 block w-full rounded-md px-3 py-1.5 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 
-                    placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-900
-                    "/>
-                </div>
-                <div class="">
-                    <label for="password" class=" block text-sm/6 font-medium text-gray-900">Password</label>
-                    <input type="password" name='password' required placeholder='password' class="
-                    mt-1 block w-full rounded-md px-3 py-1.5 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 
-                    placeholder:text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-blue-900
-                    "/>
-                </div>
+                
                 <x-button type="submit" text='Login' buttonClass='w-full justify-center mt-6'/>
             </form>
-            <p class='text-red-600 text-sm mt-1'>{{$errors->first('username')}}</p>
     
 
         </div>
