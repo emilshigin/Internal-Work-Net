0f7429581a8e6a6f0eb21699d9bb774df3669078
emilshigin <emilshigin@protonmail.com>
2025-06-02 10:12:51 -0700

    side nav

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,16 @@
+<aside class="w-44 bg-white shadow-md p-4 hidden md:flex flex-col">
+<h2 class="text-xl font-bold mb-6">Dashboard</h2>
+<nav class="space-y-2">
+    <a href="#" class="block py-2 px-3 rounded hover:bg-gray-200 text-gray-700">Home</a>
+    <a href="#" class="block py-2 px-3 rounded hover:bg-gray-200 text-gray-700">Analytics</a>
+    <a href="#" class="block py-2 px-3 rounded hover:bg-gray-200 text-gray-700">Settings</a>
+</nav>
+
+<form action="{{ route('logout') }}" method="post" class="mt-auto">
+       @csrf
+       <button type="submit" class="w-full  text-start mt-2 block py-2 px-3 rounded hover:bg-gray-200 text-red-500">
+               Logout
+       </button>            
+</form>
+
+</aside>
