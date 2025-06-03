8bb390bc6cde102f761d5d7e79c843a5deacd0d1
emilshigin <emilshigin@protonmail.com>
2025-06-03 16:31:59 -0700

    bg white css

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -16,7 +16,8 @@
     placeholder='{{ucfirst($name)}}' 
     class="
         mt-1 block w-full 
-        rounded-md px-3 py-1.5 
+        rounded-md px-3 py-1.5
+        bg-white 
         text-gray-900 outline-1 -outline-offset-1 outline-gray-300 
         placeholder:text-gray-400 focus:outline-2 
         focus:outline-offset-2 focus:outline-blue-900
