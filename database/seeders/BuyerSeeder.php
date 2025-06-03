516be73127b3f908083c3e58fc435fb838106206
emilshigin <emilshigin@protonmail.com>
2025-06-03 16:02:21 -0700

    seeders and facories

--- database/seeders/BuyerSeeder.php
+++ database/seeders/BuyerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Buyer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class BuyerSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        Buyer::factory()->count(11)->create();
     }
 }
--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,22 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // clear existing
+        Product::truncate();
+
+        // list of device
+        $products = [
+            ['name' => 'Focus'],
+            ['name' => 'G2'],
+            ['name' => 'Neo 2'],
+            ['name' => 'G3'],
+            ['name' => 'Neo 3'],
+        ];
+
+        // Create device
+        foreach($products as $product){
+            Product::create($product);
+        }
+
     }
 }
