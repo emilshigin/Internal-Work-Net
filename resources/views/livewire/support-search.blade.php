c7fb1271008b2a2d06663ac3b4f914d161f9724b
emilshigin <emilshigin@protonmail.com>
2025-06-10 18:14:30 -0700

    if empty say no match

--- resources/views/livewire/support-search.blade.php
+++ resources/views/livewire/support-search.blade.php
@@ -1,9 +1,11 @@
 <div>
     <x-input name='search | Name | Serial Number | Email | Phone' showLabel='false' isLiveSearch='true' />
-
-    <ul class=" mt-2">
-        @foreach($buyers as $buyer)
-            <li>Customer: {{ $buyer->name }}</li>
-        @endforeach
+    
+    <ul class="mt-2">
+        @forelse($buyers as $buyer)
+            <li wire:key="buyer-{{ $buyer->id }}">Customer: {{ $buyer->name }}</li>
+        @empty
+            <li class="text-gray-500 italic ml-20">No matching buyers found.</li>
+        @endforelse
     </ul>
 </div>
