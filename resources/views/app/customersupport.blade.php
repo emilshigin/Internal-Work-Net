e0d5fb630eb90c4f3bd1f7034e5984242a871c63
emilshigin <emilshigin@protonmail.com>
2025-06-12 17:39:24 -0700

    updated template

--- resources/views/app/customersupport.blade.php
+++ resources/views/app/customersupport.blade.php
@@ -1,5 +1,11 @@
 <x-app-layout tabTitle='Customer Support' bodyClass=''>
-    <div class="w-90">
-        @livewire('support-search')
+    <div class="flex gap-4">
+        <div class="w-1/4 max-w-md">
+            @livewire('support-search')
+        </div>
+        <div class="flex-1">
+            @livewire('support-search-selected')
+        </div>
     </div>
+
 </x-app-layout>
