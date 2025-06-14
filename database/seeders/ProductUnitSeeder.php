3d8d0da45f6cb939e76c8bff679124ad3be794dd
emilshigin <emilshigin@protonmail.com>
2025-06-13 18:05:43 -0700

    creadted facorty and seeders

--- database/seeders/ProductUnitSeeder.php
+++ database/seeders/ProductUnitSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ProductUnit;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,9 @@ class ProductUnitSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        fake()->unique(true);
+        $count = 500;
+        ProductUnit::factory()->count($count)->create();
+
     }
 }
