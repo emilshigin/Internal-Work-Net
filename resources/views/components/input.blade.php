8f33f95c2603eac17c057e9e10678143c41ed3b6
emilshigin <emilshigin@protonmail.com>
2025-06-13 18:01:33 -0700

    remove margin top

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -18,7 +18,7 @@
     @if ($isRequired) required @endif 
     placeholder='{{ucfirst($name)}}' 
     class="
-        mt-1 block w-full 
+        block w-full 
         rounded-md px-3 py-1.5
         bg-white 
         text-gray-900 outline-1 -outline-offset-1 outline-gray-300 
