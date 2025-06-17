625eafdc3552100fe9aa814df971fdaf523285e4
emilshigin <emilshigin@protonmail.com>
2025-06-16 18:02:19 -0700

    responsive height

--- resources/views/livewire/support-search.blade.php
+++ resources/views/livewire/support-search.blade.php
@@ -2,10 +2,10 @@
     use Illuminate\Support\Str;
 @endphp
 
-<div>
+<div class=" bg-red-300 h-12/12">
     <x-input name='Name | Serial Number | Email | Phone' showLabel='false' isLiveSearch='true' />
     
-    <ul class="mt-2 overflow-auto max-h-150 outline-1 -outline-offset-1 outline-gray-300">
+    <ul class="mt-2 overflow-auto outline-1 -outline-offset-1 outline-gray-300">
         @foreach($buyers as $buyer)
             <li  
                 wire:key="buyer-{{ $buyer->id }}" 
