318023c885209dfa864b71caf49fa6070eef0ba5
Emil Shigin <emilshigin@protonmail.com>
2025-06-19 00:23:15 -0700

    removed Tittle

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,4 @@
 <aside class="w-44 bg-white shadow-md p-4 hidden md:flex flex-col">
-<h2 class="text-xl font-bold mb-6">Dashboard</h2>
 <nav class="space-y-2">
     <a href="/" class="block py-2 px-3 rounded hover:bg-gray-200 text-gray-700">Dashboard</a>
     <a href="/customer_support" class="block py-2 px-3 rounded hover:bg-gray-200 text-gray-700">Support</a>
