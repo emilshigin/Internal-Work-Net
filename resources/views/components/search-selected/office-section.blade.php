6a62536ae139d81b391d9c019c530ad15f73c70b
Emil Shigin <emilshigin@protonmail.com>
2025-06-19 00:23:44 -0700

    added flex wrap

--- resources/views/components/search-selected/office-section.blade.php
+++ resources/views/components/search-selected/office-section.blade.php
@@ -2,12 +2,12 @@
 
 
 <div class=" text-3xl mt-3">Offices</div>
-<hr/>
-    <div class="flex gap-3">
+<hr />
+    <div class="flex flex-wrap gap-3">
     @if($officesWithSerials)
             <x-search-selected.office-cards :officesWithSerials="$officesWithSerials" />
         @else
             <p class="text-gray-500">No offices found.</p>
         @endif
     </div>
-<hr/>
+<hr />
