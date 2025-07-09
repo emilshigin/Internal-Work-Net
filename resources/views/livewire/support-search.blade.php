7913db395d7372e1b91fa07383cc25ce1844bfe7
emilshigin <emilshigin@protonmail.com>
2025-07-09 11:40:49 -0700

    updating searching to search tables

--- resources/views/livewire/support-search.blade.php
+++ resources/views/livewire/support-search.blade.php
@@ -11,6 +11,13 @@
 
     <ul class="mt-2 flex-1 overflow-y-auto  outline-gray-300 bg-white rounded-outline">
         @foreach($buyers as $buyer)
+            @php
+                $email = $buyer->primary_email;
+                $phone = $buyer->primary_phone;
+                $match = Str::contains(strtolower($email ?? ''), strtolower($query)) 
+                    || Str::contains($phone ?? '', $query);
+            @endphp
+
             <li  
                 wire:key="buyer-{{ $buyer->id }}" 
                 wire:click="select({{$buyer->id}})"
@@ -19,13 +26,17 @@
                  {{ $selectedId === $buyer->id ? ' bg-amber-200 border-accent' : 'bg-white' }}
                 ">
                     <b>{{ $buyer->name }}</b>
-                    @if(Str::contains(strtolower($buyer->email), strtolower($query)))
-                        <p> {{ $buyer->email }} </p>
-                    @elseif(Str::contains($buyer->phone, $query))
-                        <p> {{ $buyer->phone }} </p>
+
+                    @if($match)
+                        @if(Str::contains(strtolower($email ?? ''), strtolower($query)))
+                            <p>{{ $email }}</p>
+                        @elseif(Str::contains($phone ?? '', $query))
+                            <p>{{ $phone }}</p>
+                        @endif
                     @else
-                        <p> {{ $buyer->email ?? $buyer->phone }}</p>
+                        <p>{{ $email ?? $phone }}</p>
                     @endif
+
             </li>
         @endforeach
         
@@ -41,6 +52,7 @@
         </li>
         @endforeach
 
+        {{-- serial number --}}
         @foreach($productUnit as $unit)
         <li
             wire:click="select({{$unit->currentOffice?->buyer_id}})"
@@ -53,6 +65,53 @@
             </li>
         @endforeach
 
+        {{-- email --}}
+        @foreach($emails as $email)
+            @php
+                $contact = $email->contact;
+                $isBuyer = $contact instanceof \App\Models\Buyer;
+                $isOffice = $contact instanceof \App\Models\Office;
+            @endphp
+
+            <li
+                wire:click="{{ $isBuyer ? "select({$contact->id})" : '' }}"
+                class="
+                    p-1 mt-0.5 hover:bg-gray-200
+                    {{ $isBuyer && $selectedId === $contact->id ? ' bg-amber-200 border-accent' : 'bg-white' }}
+                    ">
+                <b>
+                    {{ $isBuyer ? $contact->name : ($isOffice ? $contact->office_name : 'Unknown') }}
+                </b>
+                <p>{{ $email->email }}</p>
+                @if ($isOffice)
+                    <p>{{ $contact->buyer->name ?? 'N/A' }}</p>
+                @endif
+            </li>
+        @endforeach
+  
+        {{-- phone --}}
+        @foreach($phone_numbers as $number)
+            @php
+                $contact = $number->contact;
+                $isBuyer = $contact instanceof \App\Models\Buyer;
+                $isOffice = $contact instanceof \App\Models\Office;
+            @endphp
+
+            <li
+                wire:click="{{ $isBuyer ? "select({$contact->id})" : '' }}"
+                class="
+                    p-1 mt-0.5 hover:bg-gray-200
+                    {{ $isBuyer && $selectedId === $contact->id ? ' bg-amber-200 border-accent' : 'bg-white' }}
+                    ">
+                <b>
+                    {{ $isBuyer ? $contact->name : ($isOffice ? $contact->office_name : 'Unknown') }}
+                </b>
+                <p>{{ $number->phone_number }}</p>
+                @if ($isOffice)
+                    <p>{{ $contact->buyer->name ?? 'N/A' }}</p>
+                @endif
+            </li>
+        @endforeach
 
     </ul>
 </div>
