a844f30f219a26789468e8e99a72f37d535cfadc
emilshigin <emilshigin@protonmail.com>
2025-07-09 18:11:48 -0700

    display info that can be copied

---
+++ resources/views/components/ui/display-info.blade.php
@@ -0,0 +1,7 @@
+@props(['id' => '','text'=>''])
+<div id="{{$id}}"
+        onclick="copyToClipboard('{{$id}}')" 
+        class="font-normal bg-gray-200 text-gray-700 overflow-hidden w-auto px-2 mr-3 rounded-outline cursor-pointer ">
+        {{ $text }} 
+    
+</div>
