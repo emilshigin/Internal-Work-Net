70a425dab21726caae0f7cdfcedc18ee0d09c42e
Emil Shigin <emilshigin@protonmail.com>
2025-07-11 01:21:10 -0700

    added check if primary phone and email

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -27,4 +27,18 @@ class Office extends Model
     {
         return $this->morphMany(ContactPhone::class, 'contact');
     }
+
+    // Primery contact info
+    public function getPrimaryEmailAttribute()
+    {
+        return $this->contactEmails->where('is_primary', true)->first()?->email 
+            ?? $this->contactEmails->first()?->email;
+    }
+
+    // Primary phone accessor
+    public function getPrimaryPhoneAttribute()
+    {
+        return $this->contactPhones->where('is_primary', true)->first()?->phone_number 
+            ?? $this->contactPhones->first()?->number;
+    }
 }
