874c112b9c3e12d39bd4962a0605c2959279df75
emilshigin <emilshigin@protonmail.com>
2025-07-10 17:22:46 -0700

    added minumum width

--- resources/views/components/ui/display-info.blade.php
+++ resources/views/components/ui/display-info.blade.php
@@ -1,7 +1,7 @@
 @props(['id' => '','text'=>''])
 <div id="{{$id}}"
         onclick="copyToClipboard('{{$id}}')" 
-        class="font-normal bg-gray-200 text-gray-700 overflow-hidden w-auto px-2 mr-3 rounded-outline cursor-pointer ">
+        class="font-normal bg-gray-200 text-gray-700 overflow-hidden w-auto px-2 mr-3 rounded-outline cursor-pointer min-w-50 ">
         {{ $text }} 
     
 </div>
